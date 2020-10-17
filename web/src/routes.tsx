7d330f28a7e3431b9814f23bce4c5eddc9eda456
Wanderson Silva <wandersonsilva768@gmail.com>
2020-10-17 03:58:51 -0300

    refactor: forçando a exibição de apenas uma rota por vez com o Switch

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 
-function Routes () {
+function Routes() {
     return (
         <BrowserRouter>
-            <Route exact path="/" component={Landing} />
-            <Route path="/app" component={OrphanagesMap} />
+            <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route path="/app" component={OrphanagesMap} />
+            </Switch>
         </BrowserRouter>
     );
 }
